c37d9d6b5012fe766022a601806f3838b82b7c77
mey yaou <meynedjmadehbiyayaou@gmail.com>
2024-07-23 14:24:03 +0100

    added loading effect

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Loading from "./components/Loading.jsx";
 import GamePage from "./components/GamePage.jsx";
 
 import "./App.css";
 
 function App() {
-  const [isLoading, setLoading] = useState(false);
+  //added loading effect
+  const [isLoading, setLoading] = useState(true);
+  useEffect(()=>{
+    const timeId=setTimeout(()=>{
+      setLoading(false)
+    },3000)
+    return ()=>{
+      clearTimeout(timeId)
+    }
+  }, []);
 
   return (
     <>
